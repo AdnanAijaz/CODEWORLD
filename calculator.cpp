bd8b3725e4cc2e7218528d5349ef031a04a5468c
AdnanAijaz <120813328+AdnanAijaz@users.noreply.github.com>
2023-01-23 16:41:58 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int num1, num2, operation, A, S, M, D;
+  cout << "enter two numbers:";
+  cin >> num1 >> num2;
+
+  cout << "you have entered: " << num1 << endl;
+       << num2 << endl;
+  cout << "press 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"<<endl;
+  cin >> operation;
+
+  if (operation == 1)
+  {
+    cout << "sum is: " << num1 + num2;
+  }
+  else if (operation == 2)
+  {
+    cout << "difference is: " << num1 - num2;
+  }
+  else if (operation == 3)
+  {
+    cout << "product is: " << num1 * num2;
+  }
+  else if (operation == 4)
+  {
+    cout << "quotient is: " << num1 / num2;
+  }
+  else
+  {
+    cout << "you have entered an invalid operation";
+  }
+
+  return 0;
+}
